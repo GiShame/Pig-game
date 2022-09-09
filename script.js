a3752e5af82f125fc6a12a0f468ada3d7dd8d6f4
Nikita <105992775+GiShame@users.noreply.github.com>
2022-09-09 16:31:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const dice = document.querySelector('.dice')
+const btnRoll = document.querySelector('.btn--roll')
+let current0 = document.querySelector('#current--0').textContent
+const btnHold = document.querySelector('.btn--hold')
+const score0 = document.querySelector('#score--0')
+
+dice.classList.add('hidden')
+btnRoll.addEventListener('click', function() {
+    const roll = Math.trunc(Math.random() * 6) + 1
+    console.log(roll)
+    dice.src = `Images/dice-${roll}.png`
+    dice.classList.remove('hidden')
+    if (roll > 1) {
+        current0 = +current0 + roll
+    } else {
+        current0 = 0
+    }
+})
+
+btnHold.addEventListener('click', function() {
+    if (current0 > 0) {
+        score0.textContent = current0
+    }
+})
