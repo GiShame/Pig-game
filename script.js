1ae591ff486b37332685f1c85b428dc8fbe843ae
Nikita <105992775+GiShame@users.noreply.github.com>
2022-09-15 16:54:54 +0300

    To be continued

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 'use strict';
 
+const player0 = document.querySelector('.player--0')
+const player1 = document.querySelector('.player--1')
+const current0 = document.querySelector('#current--0')
+const current1 = document.querySelector('#current--1')
+const score0 = document.querySelector('#score--0')
+const score1 = document.querySelector('#score--1')
+
 const dice = document.querySelector('.dice')
 const btnRoll = document.querySelector('.btn--roll')
-let current0 = document.querySelector('#current--0').textContent
 const btnHold = document.querySelector('.btn--hold')
-const score0 = document.querySelector('#score--0')
+
+let currentScore = 0;
+let activePlayer = 0;
+let activeScore = 0;
+const mainScore = [0, 0]
+const switchPlayer = function () {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0
+        activePlayer = activePlayer === 0 ? 1 : 0
+        currentScore = 0
+        player0.classList.toggle('player--active')
+        player1.classList.toggle('player--active')
+}
 
 dice.classList.add('hidden')
 btnRoll.addEventListener('click', function() {
@@ -12,15 +29,16 @@ btnRoll.addEventListener('click', function() {
     console.log(roll)
     dice.src = `Images/dice-${roll}.png`
     dice.classList.remove('hidden')
-    if (roll > 1) {
-        current0 = +current0 + roll
+    if (roll !== 1) {
+        currentScore += roll
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore
     } else {
-        current0 = 0
+        switchPlayer();
     }
 })
 
 btnHold.addEventListener('click', function() {
-    if (current0 > 0) {
-        score0.textContent = current0
-    }
+    mainScore[activePlayer] += currentScore
+    document.querySelector(`#score--${activePlayer}`).textContent = mainScore[activePlayer]
+    switchPlayer();
 })
