49b6770d53cc19bfd5aba041193cf0904c3c7a35
Nikita <105992775+GiShame@users.noreply.github.com>
2022-09-19 12:45:08 +0100

    Completed game

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ const score1 = document.querySelector('#score--1')
 const dice = document.querySelector('.dice')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
+const btnNew = document.querySelector('.btn--new')
 
+let playing = true;
 let currentScore = 0;
 let activePlayer = 0;
 let activeScore = 0;
-const mainScore = [0, 0]
+let mainScore = [0, 0]
 const switchPlayer = function () {
     document.querySelector(`#current--${activePlayer}`).textContent = 0
         activePlayer = activePlayer === 0 ? 1 : 0
@@ -25,6 +27,7 @@ const switchPlayer = function () {
 
 dice.classList.add('hidden')
 btnRoll.addEventListener('click', function() {
+    if (playing) {
     const roll = Math.trunc(Math.random() * 6) + 1
     console.log(roll)
     dice.src = `Images/dice-${roll}.png`
@@ -35,10 +38,36 @@ btnRoll.addEventListener('click', function() {
     } else {
         switchPlayer();
     }
+}
 })
 
 btnHold.addEventListener('click', function() {
+    if (playing) {
+    // writing into a score
     mainScore[activePlayer] += currentScore
     document.querySelector(`#score--${activePlayer}`).textContent = mainScore[activePlayer]
-    switchPlayer();
+    // checking for win
+    if (mainScore[activePlayer] >= 15) {
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+        playing = false
+        dice.classList.add('hidden')
+    } else {
+        switchPlayer()
+    }
+    }
+})
+
+btnNew.addEventListener('click', function() {
+    current0.textContent = 0;
+    current1.textContent = 0;
+    score0.textContent = 0;
+    score1.textContent = 0;
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner')
+    currentScore = 0;
+    activeScore = 0;
+    activePlayer = 0;
+    player0.classList.add('player--active')
+    playing = true
+    mainScore = [0, 0]
 })
